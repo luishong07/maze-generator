5bca5a268b396e3036007be2fb756aa077054dd0
Luis Hong <luishong50@gmail.com>
2023-03-09 14:13:19 -0600

    implemented checking adjacent cells and changing them as "visited"  and changing color on the grid

--- Cell.js
+++ Cell.js
@@ -1,27 +1,73 @@
+const index = (i, j) => {
+    if (i < 0 || j < 0 || i > cols - 1 || j > cols - 1) {
+        return -1;
+    }
+    let index = i + j * cols;
+    return index;
+};
+
 class Cell {
     constructor(i, j) {
         this.i = i;
         this.j = j;
         this.walls = [true, true, true, true];
+        this.visited = false;
+    }
+
+    checkNeighbors() {
+        let neighbors = [];
+        // let index = i + (j - 1) * cols;
+        let top = grid[index(this.i, this.j - 1)];
+        let right = grid[index(this.i + 1, this.j)];
+        let bottom = grid[index(this.i, this.j + 1)];
+        let left = grid[index(this.i - 1, this.j)];
+        // console.log(index(this.i, this.j - 1))
+        // noLoop();
+        if (top && !top.visited) {
+            neighbors.push(top);
+        }
+        if (right && !right.visited) {
+            neighbors.push(right);
+        }
+        if (bottom && !bottom.visited) {
+            neighbors.push(bottom);
+        }
+        if (left && !left.visited) {
+            neighbors.push(left);
+        }
+        // console.log(neighbors.length)
+        if (neighbors.length > 0) {
+            let r = floor(random(0, neighbors.length));
+            return neighbors[r];
+        } else {
+            return undefined;
+        }
     }
 
     show() {
         let x = this.i * w;
         let y = this.j * w;
         stroke(255);
-        noFill();
-        // rect(x,y,w,w)
-        if (this.walls[0]) {//top
+
+        if (this.walls[0]) {
+            //top
             line(x, y, x + w, y);
         }
-        if (this.walls[1]) {//right
+        if (this.walls[1]) {
+            //right
             line(x + w, y, x + w, y + w);
         }
-        if (this.walls[2]) {//bottom
+        if (this.walls[2]) {
+            //bottom
             line(x + w, y + w, x, y + w);
         }
-        if (this.walls[3]) {//left
+        if (this.walls[3]) {
+            //left
             line(x, y + w, x, y);
+        }
+        if (this.visited) {
+            fill(255, 0, 0);
+            rect(x, y, w, w);
         }
     }
 }
--- sketch.js
+++ sketch.js
@@ -3,6 +3,7 @@ let rows;
 
 let w = 40;
 let grid = [];
+let current;
 
 function setup() {
     createCanvas(400, 400);
@@ -15,12 +16,21 @@ function setup() {
             grid.push(cell);
         }
     }
+    current = grid[0];
 }
 
 function draw() {
     // put drawing code here
+
     background(60);
     for (let i = 0; i < grid.length; i++) {
         grid[i].show();
+    }
+    current.visited = true;
+    let next = current.checkNeighbors();
+    // console.log(next)
+    if (next) {
+        next.visited = true;
+        current = next;
     }
 }
